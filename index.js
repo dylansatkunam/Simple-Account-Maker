a253ec356e7201d9e921f61e9c907c5907eeac5a
dylansatkunam <113308889+dylansatkunam@users.noreply.github.com>
2023-12-06 11:34:10 -0600

    Update index.js

--- index.js
+++ index.js
@@ -15,31 +15,21 @@ function arrToPar(arr){ //For JS-Array --> HTML content
 
 
 addAccountButton.addEventListener('click', function(){
+    //grab all info from form
     var theName = myName.value;
     var theAge = myAge.value;
     var theEmail = myEmail.value;
     const account = {firstName:theName, age:theAge, email:theEmail};
+    //add new account to accounts array
     accounts.push(account);
     var paragraph = document.createElement('p');
     paragraph.classList.add('paragraph-styling');
-    paragraph.innerText = JSON.stringify(account.firstName+", "+account.age+", "+account.email); 
-    accountContainer.appendChild(paragraph);
+    paragraph.innerText = JSON.stringify(account.firstName+", "+account.age+", "+account.email); //grab all info for distribution
+    accountContainer.appendChild(paragraph);//show the new account on screen
     account.value = "";
-    console.log(accounts);
+    console.log(accounts);//log all accounts on the console
 } )
 
 
-
-// document.getElementById("submit").onclick = function() {
-// var myName = document.getElementById("myName").value;
-// var myAge = document.getElementById("myAge").value;
-// var myEmail = document.getElementById("myEmail").value;
-// const account = {firstName:myName, age:myAge, email:myEmail};
-// accounts.push(account);
-// var paragraph = document.createElement('p')
-// paragraph.innerText = myName.value; 
-// accountContainer.appendChild(paragraph);
-// console.log(accounts);
-// } 
 
 
